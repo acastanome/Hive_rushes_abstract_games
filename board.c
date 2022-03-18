9f8ca6b6b79be186db7dfce151d96a6625033955
Alba Castano <acastano@student.hive.fi>
2022-03-18 13:09:59 +0200

    cleaned board.c file, added ft_play() prototype to santorini.h. TO DO: combine board.c and turn.c to same file

--- board.c
+++ board.c
@@ -6,17 +6,12 @@
 /*   By: acastano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 14:34:30 by acastano          #+#    #+#             */
-/*   Updated: 2022/03/17 23:06:23 by acastano         ###   ########.fr       */
+/*   Updated: 2022/03/18 13:08:44 by acastano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "santorini.h"
-#include <unistd.h>
-#include <stdio.h>//printf
-
-//-------------------------------------------------------------------------
-//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 /*
  * ft_move() takes an int array representing the board (and it's content), an
@@ -27,108 +22,62 @@
 int	ft_move(int *board, int player_id, int pos1, int pos2)
 {
 	if (is_player_id(board[pos1], player_id) == 0)
-	{
 		error("Player not there");
-		return (0);
-	}
-	printf("hello1\n");
-	if (is_position_buildable(board[pos2]) == 0)
-	{
+	else if (is_position_buildable(board[pos2]) == 0)
 		error("Occupied");
-		return (0);
-	}
-	printf("hello2\n");
-	if (is_adjacent(pos1, pos2) == 0)
-	{
+	else if (is_adjacent(pos1, pos2) == 0)
 		error("Not adjacent");
-		return (0);
-	}
-	printf("hello3\n");
-	if (is_level_ok(pos1, pos2) == 0)
-	{
+	else if (is_level_ok(pos1, pos2) == 0)
 		error("Level height");
-		return (0);
+	else
+	{
+		board[pos1] = (board[pos1] - player_id);
+		board[pos2] = (board[pos2] + player_id);
+		return (1);
 	}
-	printf("hello4\n");
-	board[pos1] = (board[pos1] - player_id);
-	board[pos2] = (board[pos2] + player_id);
-	return (1);
+	return (0);
 }
 
+/*
+ * ft_build() takes an int array representing the board (and it's content),
+ * an int representing the index where the character was moved to, and
+ * an int representing the index of the square we want to build in.
+ * Checks if the player is next to the square you want to build in,
+ * and if that square is buildable. If so, it builds there.
+ * Return values: Returns 0 (and calls error) if failed, 1 if it builds.
+ */
 int	ft_build(int *board, int player_position, int position)
 {
 	if (is_adjacent(player_position, position) == 0)
-	{
 		error("Not adjacent");
-		return (0);
-	}
-	if (is_position_buildable(board[position]) == 0)
-	{
-		printf("pos build prob\n");
+	else if (is_position_buildable(board[position]) == 0)
 		error ("Occupied");
-		return (0);
+	else
+	{
+		board[position] = board[position] + 1;
+		return (1);
 	}
-	board[position] = board[position] + 1;
-	return (1);
+	return (0);
 }
 
-int	main(void)
-{
 /*
-	char	player_A = 'A';
-	char	player_B = 'B';
-	char	player_C = 'C';
-	char	player_D = 'D';
-*/
-
-/* 		Board						Array
- * 1	2	3	4	5			0	1	2	3	4
- * 6	7	8	9	10			5	6	7	8	9
- * 11	12	13	14	15
- * 16	17	18	19	20
- * 21	22	23	24	25
+ * ft_play() takes an int array representing the board (and it's content),
+ * an int representing the player_id, and three int for the indexes where the character
+ * wants to move from,  move to, and the square to build in.
+ * Calls functions ft_move() and ft_build() to do their namesake actions, and updates the
+ * board upon success.
+ * Return values: Returns 1 upon success, 0 otherwise.
  */
+int	ft_play(int *board, int player_id, int i_move_from, int i_move_to, int i_build)
+{
+	int	*temp_board;
 
-	int	board[25];
-	int	i;
-
-	i  = 0;
-	while (i < 25)
-	{
-		board[i] = 0;
-		i++;
-	}
-
-	board[3] = 10;
-	i = 0;
-	while (i < 25)
-	{
-		printf("%d, ", board[i]);
-		if ((i + 1) % 5 == 0)
-			printf("\n");
-		i++;
-	}
-	printf("\n");
-
-	ft_build(board, 3, 4);
-	printf("1. Value after building in empty square is %d\n", board[4]);
-	ft_build(board, 3, 4);
-	printf("2. Value after building in empty square is %d\n", board[4]);
-	ft_build(board, 3, 4);
-	printf("3. Value after building in empty square is %d\n", board[4]);
-
-	i  = 0;
-	while (i < 25)
-	{
-		printf("%d, ", board[i]);
-		if ((i + 1) % 5 == 0)
-			printf("\n");
-		i++;
-	}
-	printf("\n");
-
-	printf("4. Board, 10, 3, 2\nValue before moving from pos1 (%d) to pos2 (%d)\n", board[3], board[2]);
-	ft_move(board, 10, 3, 2);
-	printf("5. Value after moving from pos1 (%d) to pos2 (%d)\n", board[3], board[2]);
-	return (0);
+	if ((ft_move(temp_board, player_id, i_move_from, i_move_to) == 1)
+		&& (ft_build(temp_board, i_move_to, i_build) == 1))
+		board = temp_board;
+	else
+		return (0);
+	//print_map(map);
+	//ask_next_move;
+	return (1);
 }
--- santorini.h
+++ santorini.h
@@ -6,7 +6,7 @@
 /*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 15:22:23 by spuustin          #+#    #+#             */
-/*   Updated: 2022/03/18 11:42:43 by spuustin         ###   ########.fr       */
+/*   Updated: 2022/03/18 12:56:18 by acastano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,19 @@
 //error.c
 int error(char *str);
 
-//check_functions
-int	is_player(int square_status);
-int	is_player_id(int square_status, int player_id);
-int	is_adjacent(int pos1, int pos2);
-int	height_difference(int pos1, int pos2);
-int	is_level_ok(int pos1, int pos2);
-int	is_dome(int square_status);
-int	is_position_buildable(int position);
-
 //board.c
+int	ft_play(int *board, int player_id, int i_move_from, int i_move_to, int i_build);
 int	ft_move(int *board, int player_id, int pos1, int pos2);
 int	ft_build(int *board, int player_position, int position);
+
+//check_functions
+int	is_player_id(int square_status, int player_id);
+int	is_player(int square_status);
+int	is_position_buildable(int position);
+int	is_adjacent(int pos1, int pos2);
+int	is_level_ok(int pos1, int pos2);
+int	height_difference(int pos1, int pos2);
+int	is_dome(int square_status);
 
 //print
 void	print_state(int *array);
